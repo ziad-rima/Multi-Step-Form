4acd1e8ed9bc76797bb7e3e2de9f22f79999f361
Ziad Rima <ziadrima49@gmail.com>
2025-03-18 10:22:37 +0100

    create routes to navigate pages

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import Step1 from "./pages/Step1";
+import Step2 from "./pages/Step2";
+import Step3 from "./pages/Step3";
+import Step4 from "./pages/Step4";
+import Step5 from "./pages/Step5";
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Step1 />} />
+      <Route path="/step-2" element={<Step2 />} />
+      <Route path="/step-3" element={<Step3 />} />
+      <Route path="/step-4" element={<Step4 />} />
+      <Route path="/step-5" element={<Step5 />} />
+    </Routes>
+  )
+}
+
+export default App
