aa607662fba791b654314a8e8c5a22e58bcced6e
Ziad Rima <ziadrima49@gmail.com>
2025-03-20 10:29:08 +0100

    wrap the app component with the FormProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom"
+import { FormProvider } from './context/FormContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <FormProvider>
+        <App />
+      </FormProvider>
     </BrowserRouter> 
   </StrictMode>
 )
