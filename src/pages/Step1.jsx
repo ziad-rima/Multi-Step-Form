dacea21602d1edd28747145d74f8658dfd7e4e7e
Ziad Rima <ziadrima49@gmail.com>
2025-03-20 10:27:29 +0100

    add navigation to other pages & use the context to store input

--- src/pages/Step1.jsx
+++ src/pages/Step1.jsx
@@ -1,8 +1,48 @@
+import { useNavigate } from "react-router-dom"
+import { useFormContext } from "../context/FormContext";
 const Step1 = () => {
+  const { formData, updateFormData } = useFormContext();
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    updateFormData({[e.target.name]: e.target.value});
+  }
+
   return (
-    <h1>
-      Step 1: Personal Info
-    </h1>
+    <>
+      <h1>Step 1: Personal Info</h1>
+      <p>Please provide your name, email address, and phone number.</p>
+
+      <label htmlFor="name">Name</label>
+      <input 
+        type="text"
+        id="name"
+        name="name"
+        placeholder="e.g. Stephen King"
+        value={formData.name}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="email">Email Address</label>
+      <input 
+        type="email" 
+        id="email"
+        name="email"
+        placeholder="e.g. stephenking@lorem.com"
+        value={formData.email}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="phone-number">Phone Number</label>
+      <input 
+        type="tel" 
+        id="phone-number"
+        name="phone"
+        value={formData.phone}
+        onChange={handleChange}
+      />
+      <button onClick={() => navigate("/step-2")}>Next Step</button>
+    </>
   )
 }
 
