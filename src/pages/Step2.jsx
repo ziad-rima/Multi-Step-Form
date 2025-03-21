7e64d6e70c5208a072b907dba5e8a78d1bfbe6b0
Ziad Rima <ziadrima49@gmail.com>
2025-03-21 09:39:12 +0100

    import and use PlanSelection component

--- src/pages/Step2.jsx
+++ src/pages/Step2.jsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom"
-
+import PlanSelection from "../components/PlanSelection";
 const Step2 = () => {
-  const navigate = useNavigate();  
+  const navigate = useNavigate();
   return (
     <>
-      <h1>Step 2: Select Plan</h1>
+      <h1>Step 2: Select your plan</h1>
+      <p>You have the option of monthly or yearly billing.</p>
+      <PlanSelection />
       <button onClick={() => navigate("/step-3")}>Next Step</button>
       <button onClick={() => navigate("/")}>Go Back</button>
     </>
