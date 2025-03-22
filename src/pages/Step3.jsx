d32ee7729bed6bcaae2a941b52b629ac34e4a5f8
Ziad Rima <ziadrima49@gmail.com>
2025-03-22 10:07:53 +0100

    import and use AddOnsSelections component

--- src/pages/Step3.jsx
+++ src/pages/Step3.jsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom"
-
+import AddOnsSelections from "../components/AddOnsSelections";
 const Step3 = () => {
   const navigate = useNavigate();
   return (
     <>
       <h1>Step 3: Pick Add-ons</h1>
+      <p>Add-ons help enhance your gaming experience.</p>
+      <AddOnsSelections />
       <button onClick={() => navigate("/step-4")}>Next Step</button>
       <button onClick={() => navigate("/step-2")}>Go Back</button>
     </>
