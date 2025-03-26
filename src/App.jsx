1990671d2d3666cbd0b50d62ecb14636a37b42ab
Ziad Rima <ziadrima49@gmail.com>
2025-03-26 11:18:00 +0100

    include the step tracker in all pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Step2 from "./pages/Step2";
 import Step3 from "./pages/Step3";
 import Step4 from "./pages/Step4";
 import Step5 from "./pages/Step5";
+import StepTracker from "./components/StepTracker";
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Step1 />} />
-      <Route path="/step-2" element={<Step2 />} />
-      <Route path="/step-3" element={<Step3 />} />
-      <Route path="/step-4" element={<Step4 />} />
-      <Route path="/step-5" element={<Step5 />} />
-    </Routes>
+    <div>
+      <StepTracker />
+      <Routes>
+        <Route path="/" element={<Step1 />} />
+        <Route path="/step-2" element={<Step2 />} />
+        <Route path="/step-3" element={<Step3 />} />
+        <Route path="/step-4" element={<Step4 />} />
+        <Route path="/step-5" element={<Step5 />} />
+      </Routes>
+    </div>
   )
 }
 
