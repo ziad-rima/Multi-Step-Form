089d4b924d388529c875e726fe27ebb6fa0639d9
Ziad Rima <ziadrima49@gmail.com>
2025-04-08 12:38:07 +0200

    add svg components to plans object | add toggle switch functionality

--- src/components/PlanSelection.jsx
+++ src/components/PlanSelection.jsx
@@ -1,13 +1,15 @@
 import { useFormContext } from "../context/FormContext"
-
+import ArcadeSVG from "./ArcadeSVG";
+import AdvancedSVG from "./AdvancedSVG";
+import ProSVG from "./ProSVG";
 const PlanSelection = () => {
   
     const {selectedPlan, setSelectedPlan, isYearly, toggleBilling} = useFormContext();
   
     const plans = [
-        {name: "Arcade", monthlyPrice: 9, yearlyPrice: 90},
-        {name: "Advanced", monthlyPrice: 12, yearlyPrice: 120},
-        {name: "Pro", monthlyPrice: 15, yearlyPrice: 150},
+        {name: "Arcade", monthlyPrice: 9, yearlyPrice: 90, icon: <ArcadeSVG />},
+        {name: "Advanced", monthlyPrice: 12, yearlyPrice: 120, icon: <AdvancedSVG />},
+        {name: "Pro", monthlyPrice: 15, yearlyPrice: 150, icon: <ProSVG />},
     ];
 
     const handlePlanChange = (plan) => {
@@ -27,16 +29,36 @@ const PlanSelection = () => {
                     name="plan"
                     checked={selectedPlan.name === plan.name}
                     onChange={() => handlePlanChange(plan)}
+                    className="plan-selection-input"
                 />
-                <label>{plan.name} - ${isYearly ? plan.yearlyPrice : plan.monthlyPrice}/{isYearly ? "yr" : "mo"}</label>
+                <div className="icon">
+                    {plan.icon}
+                </div>
+                <div className="label-plan">
+                <label htmlFor={plan.name} className="label-of-plan ubuntu-medium">
+                    {plan.name}
+                </label>
+                    <p className="plan ubuntu-regular">${isYearly ? plan.yearlyPrice : plan.monthlyPrice}/{isYearly ? "yr" : "mo"}</p>
+
+                </div>
+                {isYearly && <p className="free-months ubuntu-medium">2 months free</p>}
             </div>
             ))}
         </div>
 
         <div className="toggle-plan-container">
-            <span>Monthly</span>
-            <button onClick={toggleBilling}>{isYearly ? "Switch to Monthly" : "Switch to Yearly"}</button>
-            <span>Yearly</span>
+            <span className={`span-month ubuntu-medium ${!isYearly ? "is-active" : ""}`}>Monthly</span>
+
+            <label className="switch">
+                <input
+                type="checkbox"
+                checked={isYearly}
+                onChange={toggleBilling}
+                />
+                <span className="slider" />
+            </label>
+
+            <span className={`span-year ubuntu-medium ${isYearly ? "is-active" : ""}`}>Yearly</span>
         </div>
     </div>
   )
