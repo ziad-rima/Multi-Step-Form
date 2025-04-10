62f88b1b14ba0e361b06de11a5b7863293a71a52
Ziad Rima <ziadrima49@gmail.com>
2025-04-10 11:00:16 +0200

    add checkmark and style the selected addon

--- src/components/AddOnsSelections.jsx
+++ src/components/AddOnsSelections.jsx
@@ -1,59 +1,62 @@
 import { useFormContext } from "../context/FormContext"
-
+import CheckmarkSVG from "./CheckmarkSVG";
 const AddOnsSelections = () => {
-
+    
     const {formData, updateFormData, isYearly} = useFormContext();
-
+    
     const addOns = [
         { 
             name: "Online service", 
             description: "Access to multiplayer games",
-            price: isYearly ? 10 : 1,
+            monthlyPrice: 1,
+            yearlyPrice: 10
         }, 
         {
             name: "Larger storage", 
             description: "Extra 1TB of cloud save",
-            price: isYearly ? 20 : 2,
+            monthlyPrice: 2,
+            yearlyPrice: 20
         },
         {
             name: "Customizable profile", 
             description: "Custom theme on your profile",
-            price: isYearly ? 20 : 2,
+            monthlyPrice: 2,
+            yearlyPrice: 20
         }
     ]
-
+    
     const handleCheckbox = (addon) => {
         const isSelected = formData.addOns.some((item) => item.name === addon.name);
+        
         const updatedAddons = isSelected 
-            ? formData.addOns.filter((item) => item.name != addon.name)
-            : [...formData.addOns, addon];
-
+        ? formData.addOns.filter((item) => item.name != addon.name)
+        : [...formData.addOns, addon];
+        
         updateFormData({addOns: updatedAddons});
     }
-
+    
     return (
-    <div className="addons-selection-container">
-        <div className="addons">
-            {addOns.map((addon) => (
-                <div 
+        <div className="addons-selection-container">
+            <div className="addons">
+                {addOns.map((addon) => (
+                    <div 
                     key={addon.name} 
-                    className="addon"
+                    className={`addon ${formData.addOns.some(item => item.name == addon.name) ? "selected-addon" : ""}`}
                     onClick={() => handleCheckbox(addon)}
-                    style={{
-                        border: formData.addOns.some((item) => item.name === addon.name) ? "2px solid blue" : "1px solid gray",
-                        padding: "10px",
-                        cursor: "pointer"
-                    }}
-                >
-                   <h3>{addon.name}</h3>
-                   <p>{addon.description}</p>
-                   <p>+${addon.price}/{isYearly ? "yr" : "mo"}</p>
-                </div>
-            ))}
+                    >
+                    <div className={`checkmark-container ${formData.addOns.some((item) => item.name === addon.name) ? "checked" : ""}`}>
+                        <CheckmarkSVG />
+                    </div>
+                    <div className="name-description">
+                        <h3 className="addon-name ubuntu-medium">{addon.name}</h3>
+                        <p className="addon-description ubuntu-regular">{addon.description}</p>
+                    </div>
+                        <p className="addon-price ubuntu-regular">+${addon.price}/{isYearly ? "yr" : "mo"}</p>
+                    </div>
+                ))}
+            </div>
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default AddOnsSelections
