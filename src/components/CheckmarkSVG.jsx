f9496bf5c85c9312bb6b4638efe821ec2589a2fc
Ziad Rima <ziadrima49@gmail.com>
2025-04-10 10:56:41 +0200

    create components for checkmark and thank you SVGs

---
+++ src/components/CheckmarkSVG.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+const CheckmarkSVG = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={12}
+    height={9}
+    viewBox="0 0 12 9"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="#FFF"
+      strokeWidth={2}
+      d="m1 4 3.433 3.433L10.866 1"
+    />
+  </svg>
+);
+export default CheckmarkSVG;
